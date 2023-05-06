4bd46343df7d6325a52f0bcf30de75a7ce3a3d80
Nishargo Nigar <penalty_shoot_out@yahoo.com>
2023-05-06 13:52:34 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,6 +1,12 @@
+# import math library
+import math
+
 # function to get the absolute value of a number is abs ()
 
 integer = -60
 print('Absolute value of the given integer is', abs(integer))
+
+# function to return the value (in float) of the first parameter and the sign of the second parameter
+print(math.copysign(3,-98.4) #This should return -3.0
 
 
