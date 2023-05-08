328e5c1cedb31eb8f62b9f07f7a7fea1e02fd3ff
Nishargo Nigar <penalty_shoot_out@yahoo.com>
2023-05-08 11:20:18 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -9,4 +9,7 @@ print('Absolute value of the given integer is', abs(integer))
 # function to return the value (in float) of the first parameter and the sign of the second parameter
 print(math.copysign(3,-98.4) #This should return -3.0
 
+# function to return the factorial of number, it only accepts positive integers.
+print(math.factorial(5))
+
 
