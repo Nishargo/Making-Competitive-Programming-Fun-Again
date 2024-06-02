f13ad4b9114a1003e175c052b1567f2882dee060
NN <penalty_shoot_out@yahoo.com>
2024-06-02 09:55:13 +0200

    Update prime-number.py

--- prime-number.py
+++ prime-number.py
@@ -1,1 +1,2 @@
-
+# Prime number: A number is called a prime number if it has 2 factors including the number itself and 1. It's a natural number greater than 1. 
+# 3 is a prime number because we get 3 as a product, either 1 × 3 or 3 × 1, involving 3 itself
