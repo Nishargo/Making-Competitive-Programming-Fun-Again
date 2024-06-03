b1da54465d17f0f46df8d3005b8b22431932573d
NN <penalty_shoot_out@yahoo.com>
2024-06-03 09:30:16 +0200

    Update prime-number.py

--- prime-number.py
+++ prime-number.py
@@ -1,2 +1,10 @@
 # Prime number: A number is called a prime number if it has 2 factors including the number itself and 1. It's a natural number greater than 1. 
 # 3 is a prime number because we get 3 as a product, either 1 × 3 or 3 × 1, involving 3 itself
+
+def check_if_it_is_prime(n):
+  for i in range(2,n):
+    if (n%i) == 0:
+      return False
+  return True
+  
+print(f'{check_if_it_is_prime(13)}')
