2d7a30ccbb21fc51288e12bd70d6a9277f3e0693
NN <penalty_shoot_out@yahoo.com>
2024-06-05 11:25:34 +0200

    Update Longest-Common-Subsequence.py

--- Longest-Common-Subsequence.py
+++ Longest-Common-Subsequence.py
@@ -1,3 +1,3 @@
 # A longest common subsequence (LCS) is the longest subsequence that is common in all given input sequences. 
-In a given set of sequences, the goal is to find a common subsequence of all the sequences of maximal length.
+# In a given set of sequences, the goal is to find a common subsequence of all the sequences of maximal length.
 
