6d945b02dc07f3d30d6b209b22176e217d2f1b66
NN <penalty_shoot_out@yahoo.com>
2024-06-07 09:38:04 +0200

    Update Longest-Common-Subsequence.py

--- Longest-Common-Subsequence.py
+++ Longest-Common-Subsequence.py
@@ -1,4 +1,4 @@
 # A longest common subsequence (LCS) is the longest subsequence that is common in all given input sequences. 
 # In a given set of sequences, the goal is to find a common subsequence of all the sequences of maximal length.
 # Example: "ace" is a subsequence of "abcdef".
-
+# We will use recursion to solve the problem.
