170119b6226a23fe95ca0f6134b32bce68eb9400
NN <penalty_shoot_out@yahoo.com>
2024-06-13 22:21:59 +0200

    Update Longest-Common-Subsequence.py

--- Longest-Common-Subsequence.py
+++ Longest-Common-Subsequence.py
@@ -1,8 +1,25 @@
 # A longest common subsequence (LCS) is the longest subsequence that is common in all given input sequences. 
 # In a given set of sequences, the goal is to find a common subsequence of all the sequences of maximal length.
+# Or in easier words, the goal is generating all possible subsequences and finding the longest among them that is present in both strings.
 # Example: "ace" is a subsequence of "abcdef".
 # We will use recursion to solve the problem.
 # At first, we will create a recursive function.
 # Then we will decide on the conditions based on the first characters of the strings.
 # Depending on the conditions, we will run the next recursive function.
 
+# X: First sequence (string), Y: Second sequence (string), m: Length of the first sequence X, n: Length of the second sequence Y
+def lcs(X, Y, m, n):
+ # Base case
+    if m == 0 or n == 0:
+        return 0
+      # Recursive case
+    elif X[m-1] == Y[n-1]:
+        return 1 + lcs(X, Y, m-1, n-1)
+    else:
+        return max(lcs(X, Y, m, n-1), lcs(X, Y, m-1, n))
+
+
+if __name__ == '__main__':
+    S1 = "AGGTABK"
+    S2 = "GXTXAYBL"
+    print("Length of LCS is", lcs(S1, S2, len(S1), len(S2)))
