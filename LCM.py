7f803b2c9ceb21750cb4381cac7313b519968239
NN <penalty_shoot_out@yahoo.com>
2024-06-21 20:28:36 +0200

    Update LCM.py

--- LCM.py
+++ LCM.py
@@ -1,1 +1,13 @@
+def calculate_lcm(a, b):
+    # Ensure a is the larger number
+    if a < b:
+        a, b = b, a
+    
+    # Start with the larger number as the first multiple
+    lcm = a
+    
+    while True:
+        if lcm % b == 0:
+            return lcm
+        lcm += a
 
