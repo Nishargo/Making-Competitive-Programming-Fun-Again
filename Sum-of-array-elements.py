b55d5aa23433f95d7bb6719fe00d4fde9a36c052
NN <penalty_shoot_out@yahoo.com>
2024-06-23 14:11:18 +0200

    Update Sum-of-array-elements.py

--- Sum-of-array-elements.py
+++ Sum-of-array-elements.py
@@ -1,1 +1,14 @@
+#  The function 'sum_of_array_elements' takes a list of numbers as an argument and returns the sum of its elements.
+def sum_of_array_elements(array):
 
+    # Parameter:
+    # array (list): A list of numerical elements.
+
+    # The 'sum' function is used to compute the sum of the list. 
+    
+    total_sum = sum(array)
+    return total_sum
+
+# Example
+array = [1, 2, 3, 4, 5]
+print(f"The sum of the array elements is: {sum_of_array_elements(array)}")
