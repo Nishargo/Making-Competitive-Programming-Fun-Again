e93ed8066ac5fca339774fc10d0751e09a99b0fd
NN <penalty_shoot_out@yahoo.com>
2024-08-20 10:59:04 +0200

    Update binary-search.py

--- binary-search.py
+++ binary-search.py
@@ -1,4 +1,4 @@
-# Binary search is an efficient algorithm used to find the position of a target value within a sorted array. 
+# Binary search is an efficient algorithm used to find the position of a target value within a sorted array.
 # It works by repeatedly dividing the search interval in half. This method is known as "divide-and-concquer."
 def binary_search(arr, target):
     low = 0
